4aa053b75117eda2aa5ca8f2307941febfa8be73
Cynthia Mujyambere <cm845@cornell.edu>
2024-12-03 15:19:45 -0500

    minor changes

--- src/hooks/autoLogout.js
+++ src/hooks/autoLogout.js
@@ -1,9 +1,9 @@
 // src/hooks/useAutoLogout.js
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const useAutoLogout = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     useEffect(() => {
         let timer;
@@ -13,7 +13,7 @@ const useAutoLogout = () => {
             timer = setTimeout(() => {
                 localStorage.removeItem('teacherToken');
                 localStorage.removeItem('studentToken');
-                navigate('/login');  // Redirect to login page after 10 minutes of inactivity
+                window.location.href = '/login';  // Redirect to login page after 10 minutes of inactivity
             }, 600000);  // 10 minutes in milliseconds
         };
 
