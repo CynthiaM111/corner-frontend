8c790dd6db568736aa4b73553b77c12c911b2074
Cynthia Mujyambere <cm845@cornell.edu>
2025-05-12 12:12:52 -0400

    Minor fixes for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    // output: 'export', // Outputs a Single-Page Application (SPA)
-    distDir: 'build', // Changes the build output directory to `build`
+    
     env: {
         NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
         NEXT_PUBLIC_DEV_BASE_URL: process.env.NEXT_PUBLIC_DEV_BASE_URL,
